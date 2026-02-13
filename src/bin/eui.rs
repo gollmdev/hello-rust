96890838a674a3e84c148a02ed331c97dd558df9
wangyang <1749748955@qq.com>
2026-02-13 22:24:45 +0800

    add egui

---
+++ src/bin/eui.rs
@@ -0,0 +1,51 @@
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    env_logger::init();
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+
+            ui.horizontal(|ui| {
+                ui.label("Your name:");
+                ui.text_edit_singleline(&mut self.name);
+            });
+
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
